980887f658a43077418df6cd82aa7df8c509d4d7
TheAlexGo <alex280702@mail.ru>
2021-10-09 01:13:26 +0300

    Вывод тегов в информации о пользователе

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,6 +11,15 @@
             <li class="list-group-item">Email: {{ $user->email }}</li>
             <li class="list-group-item">Created: {{ $user->created_at->format('d.m.y H:m') }}</li>
             <li class="list-group-item">Updated: {{ $user->updated_at->format('d.m.y H:m') }}</li>
+            @if($user->tags->count())
+                <li class="list-group-item">Tags:
+                    <ul class="list-group list-group-flush">
+                        @foreach($user->tags as $tag)
+                            <li class="list-group-item">{{ $tag->text }}</li>
+                        @endforeach
+                    </ul>
+                </li>
+            @endif
         </ul>
     </div>
 
